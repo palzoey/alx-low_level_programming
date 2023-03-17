97e80d26f3dfea696c940df718cf478a93a7b785
palesa <palakodisang@gmail.com>
2023-03-17 14:42:54 -0700

    Prints numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,14 +3,14 @@
 /**
  * main - single digit numbers
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 
 int main(void)
 {
 	int n;
 
-	for (n = 0 ; n <10 ; n++)
+	for (n = 0 ; n < 10 ; n++)
 		printf("%d", n);
 	print("\n");
 	return (0);
