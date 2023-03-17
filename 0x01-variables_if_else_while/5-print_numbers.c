57d8ab89e916edffdc859abf113763c6186f200b
palesa <palakodisang@gmail.com>
2023-03-17 14:47:27 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - single digit numbers
+ * main - main point
  *
  * Return: Always 0
  */
 
 int main(void)
 {
-	int n;
+        int n;
 
-	for (n = 0 ; n < 10 ; n++)
-		printf("%d", n);
-	print("\n");
-	return (0);
+        for (n = 0; n < 10; n++)
+                printf("%d", n);
+        printf("\n");
+        return (0);
 }
