3208abace7d4eddb654d68ba8e9c9d4d944b3606
palesa <palakodisang@gmail.com>
2023-03-17 14:39:06 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <10 ; n++)
+		printf("%d", n);
+	print("\n");
+	return (0);
+}
