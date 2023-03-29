f5d11b903f0d02b1ee4c5b51056926ae30562b3e
palesa <palakodisang@gmail.com>
2023-03-29 12:12:09 -0700

    Check uppercase char

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - check uppercase character
+ * @c: variable text
+ * Return: always 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
